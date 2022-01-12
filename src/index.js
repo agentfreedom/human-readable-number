29057762608e4380643bed816ce7d05016068983
agentfreedom <agentfreedom@zoho.com>
2022-01-12 19:46:36 +0200

    feat: add two "String" from writing integers to "const num" and decimal numbers to "const tens"

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 module.exports = function toReadable (number) {
-  
+    const num = "zero one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen".split(" ");
+    const tens = "twenty thirty forty fifty sixty seventy eighty ninety".split(" ");
 }
