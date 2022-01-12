b1fde9618abf4ce15f980ad5cf6447e29535b636
agentfreedom <agentfreedom@zoho.com>
2022-01-12 20:02:00 +0200

    feat: add a loop to test integers up to number = 1000, returning a substring using an empty string as a separator using the ".split (" ");"

--- src/index.js
+++ src/index.js
@@ -6,5 +6,7 @@ module.exports = function toReadable (number) {
         return num[number];
     } else if (number < 100) {
         return tens[~~(number/10)-2] + (digit? " " + num[digit]: "");
+    } else if (number < 1000) {
+        return num[~~(number/100)] +" hundred" + (number%100 == 0? "": " " + toReadable(number%100));
     }
 }
