54dc806bd98d4ff36342def7031cac5e95b09ea1
agentfreedom <agentfreedom@zoho.com>
2022-01-12 20:12:59 +0200

    feat: add "let digit" variable to determine the remainder of the division of numbers by 10

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 module.exports = function toReadable (number) {
     const num = "zero one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen".split(" ");
     const tens = "twenty thirty forty fifty sixty seventy eighty ninety".split(" ");
-
+    let digit = number % 10;
+    
     if (number < 20) { 
         return num[number];
     } else if (number < 100) {
